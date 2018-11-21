ef9165e6e75228c918c2a17c3581e43396a91f92
Kacper Zielinski <236698@student.pwr.edu.pl>
2018-11-21 20:40:02 +0100

    Added Kotlin support

---
+++ app/src/main/java/pwr/android/wrocnav/MainActivity.kt
@@ -0,0 +1,12 @@
+package pwr.android.wrocnav
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
